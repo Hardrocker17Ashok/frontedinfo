45c719fa192be0caf5a012f57feb2a3624177d6c
Ashok Sharma <ashoksharmaas9358@gmail.com>
2026-02-09 12:58:37 +0530

    api add

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 const API = "https://myinfo-iqq5.onrender.com";
 
 export const getProfile = async () => {
-  const res = await fetch(`${API}/profile`);
+  const res = await fetch(`${API}/api/profile`);
   return res.json();
 };
 
 export const getProjectsBySkill = async (skill) => {
-  const res = await fetch(`${API}/projects?skill=${skill}`);
+  const res = await fetch(`${API}/api/projects?skill=${skill}`);
   return res.json();
 };
 
 export const searchData = async (q) => {
-  const res = await fetch(`${API}/search?q=${q}`);
+  const res = await fetch(`${API}/api/search?q=${q}`);
   return res.json();
 };
